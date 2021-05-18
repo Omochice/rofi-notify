964605402377c5af0f38870b27d7719c36183641
mochi-home <mochi.mls.ntl@gmail.com>
2021-05-18 23:29:25 +0900

    fix: fix option

--- rofi-notify.sh
+++ rofi-notify.sh
@@ -3,9 +3,10 @@
 function rofi-notify {
     # Please `theme` change to your favorite
     local theme="night-owl"
-    local theme_option=$(readlink -f ${BASH_SOURCE[0]})/"$nightowl".rasi
+    local theme_option=$(dirname $(readlink -f ${BASH_SOURCE[0]}))/"$theme".rasi
+    echo $theme_option
 
-    command echo $@ | rofi -theme $theme -dmenu >/dev/null 2>&1
+    command echo $@ | rofi -theme $theme_option -dmenu >/dev/null 2>&1
 }
 
 rofi-notify $1
