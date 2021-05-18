1102ce8aa544bb4e4a2c70a7bdb9905fd20e758a
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-05-18 19:24:22 +0900

    update: add redirect to /dev/null

--- rofi-notify.sh
+++ rofi-notify.sh
@@ -3,7 +3,7 @@
 function rofi-notify {
     # Please `theme` change to your favorite
     local theme="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/night-owl.rasi"
-    command echo "$1" | rofi -theme $theme -dmenu
+    command echo "$1" | rofi -theme $theme -dmenu >/dev/null 2>&1
 }
 
 rofi-notify $1
