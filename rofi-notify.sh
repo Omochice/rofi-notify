7dceb77b5b6d7ce01fe89ca08cc55dcd074f757e
mochi-home <mochi.mls.ntl@gmail.com>
2021-05-18 23:08:04 +0900

    Update: update command

--- rofi-notify.sh
+++ rofi-notify.sh
@@ -2,8 +2,10 @@
 
 function rofi-notify {
     # Please `theme` change to your favorite
-    local theme="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/night-owl.rasi"
-    command echo "$1" | rofi -theme $theme -dmenu >/dev/null 2>&1
+    local theme="night-owl"
+    local theme_option=$(readlink -f ${BASH_SOURCE[0]})/"$nightowl".rasi
+
+    command echo $@ | rofi -theme $theme -dmenu >/dev/null 2>&1
 }
 
 rofi-notify $1
